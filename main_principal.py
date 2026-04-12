9cc541d3c6e4a1c43b12231f6c1bf29c7fed9ab2
Alanwalker005 <aewalkergarcia@udc.edu.ar>
2026-04-11 23:28:41 -0300

    Ultimos cambios

--- main_principal.py
+++ main_principal.py
@@ -41,16 +41,10 @@ while corriendo:
 
     teclas = pygame.key.get_pressed()
     tiempo_actual = pygame.time.get_ticks()
-<<<<<<< HEAD
 
-    # MOVIMIENTO
-=======
-    
-    # MOVIMIENTO JUGADOR 1
     pos_anterior_1 = grafico1.rect.copy()
-   
+    # MOVIMIENTO JUGADOR 1
 
->>>>>>> 25e0d54 (Agregue los test faltante y la actualizacion del README.md)
     if teclas[pygame.K_w]:
         grafico1.mover(0, -velocidad)
     if teclas[pygame.K_s]:
@@ -59,7 +53,11 @@ while corriendo:
         grafico1.mover(-velocidad, 0)
     if teclas[pygame.K_d]:
         grafico1.mover(velocidad, 0)
-
+    if grafico1.colisiona_con(grafico2):
+        grafico1.rect = pos_anterior_1
+    
+    # MOVIMIENTO JUGADOR 2
+    pos_anterior_2 = grafico2.rect.copy()
     if teclas[pygame.K_UP]:
         grafico2.mover(0, -velocidad)
     if teclas[pygame.K_DOWN]:
@@ -69,13 +67,11 @@ while corriendo:
     if teclas[pygame.K_RIGHT]:
         grafico2.mover(velocidad, 0)
 
-<<<<<<< HEAD
-=======
     # evitar que atraviese al otro
     if grafico2.colisiona_con(grafico1):
         grafico2.rect = pos_anterior_2
+   
 
->>>>>>> 25e0d54 (Agregue los test faltante y la actualizacion del README.md)
     # LIMITES
     grafico1.limitar_pantalla(ANCHO, ALTO)
     grafico2.limitar_pantalla(ANCHO, ALTO)
--- test/test_personaje.py
+++ test/test_personaje.py
@@ -16,7 +16,7 @@ class TestPersonaje(unittest.TestCase):
 
     #CASO BASICO DE RECIBIR_DANIO
     def test_recibir_danio(self):
-        """Verifica que recibio el daño correctamente"""
+        """Verifica que la funcion recibir daño funciona correctamenet"""
         self.personaje.recibir_danio(10)
         self.assertEqual(self.personaje.vida, 90)
 
@@ -25,54 +25,91 @@ class TestPersonaje(unittest.TestCase):
         self.personaje_sin_vida.recibir_danio(50)
         self.assertEqual(self.personaje_sin_vida.vida, 0)
 
-    #CASO LIMITE de recibir daño en exceso
+    #CASO LIMITE
     def test_recibir_danio_exceso(self):
-        """Verifica que el personaje recibio un daño en exceso"""
-        self.personaje.recibir_danio(110)
-        self.assertEqual(self.personaje.vida, 0)        #modificado
+       """Verifica que el personaje recibio demasiado daño"""
+       self.personaje.recibir_danio(110)
+       self.assertEqual(self.personaje.vida, 0)
 
-    def test_recibir_danio_exacto(self):
-        """verifica que la vida quede en 0 y no en negativo"""
-        self.personaje.recibir_danio(100)
-        self.assertEqual(self.personaje.vida, 0)
-
+    
+    def test_recibir_danio_0(self):
+        """Verifica que recibir daño 0 no cambia la vida"""
+        self.personaje.recibir_danio(0)
+        self.assertEqual(self.personaje.vida, 100)
+    
+    def test_recibir_danio_negativo(self):
+        """No debería permitir daño negativo"""
+        with self.assertRaises(ValueError):
+            self.personaje.recibir_danio(-10)
 
 #---------------------------------------------------------------------------------------------
-
     #CASO BASICO DE ESTOY_VIVO
     def test_estoy_vivo(self):
         """verificar que esta vivo"""
-        self.assertTrue(self.personaje.estoy_vivo(), True)
+        self.assertTrue(self.personaje.estoy_vivo())
 
-    #CASO BASICO DE Mostrar estado
+#------------------------------------------------------------------------------------------------------------------
+
+     #CASO BASICO DE Mostrar estado
     def test_mostrar_estado(self):
         """verificar que la funcion mostrar estado, si muestra el estado"""
         estado = self.personaje.mostrar_estado()
         self.assertEqual(estado, "La vida actual de Alan es 100")
         #return print(f"La vida actual de {self.nombre} es {self.vida}")
 
-#---------------------------------------------------------------------------
-    #CASO BASICO de morir
+#---------------------------------------------------------------------------------------------------------------------
+
     def test_morir(self):
-        """verifica que el metodo morir funcione correctamente"""
-        self.personaje.morir()
-        self.assertEqual(self.personaje.vida, 0)
-        self.assertFalse(self.personaje.estoy_vivo())
-#-----------------------------------------------------------------
-    #CASO BASICO de bloquear
-    def test_sin_bloqueo(self):
-        """Verifica que si bloqueo"""
-        self.assertFalse(self.personaje.esta_bloqueando)
+       """Verifica que la funcion morir funciona correctamente"""
+       self.personaje.morir()
+       self.assertEqual(self.personaje.vida, 0)
+       self.assertFalse(self.personaje.estoy_vivo())
+    
+
+
+#-----------------------------------------------------------------------------------------------------------------
 
     def test_bloqueo(self):
-        """Verifica que la funcion de bloqueo funcione"""
+        """Verifica que bloqueo el ataque"""
         self.assertFalse(self.personaje.esta_bloqueando)
         self.personaje.bloqueo()
         self.assertTrue(self.personaje.esta_bloqueando)
+
+    # test para verificar que bloquea y no recibe daño
+    def test_bloqueo_absorbe_danio(self):
+        """ verificar que bloqueo y no recibe daño"""
+        self.personaje.bloqueo()
+        self.personaje.recibir_danio(50)
+        self.assertEqual(self.personaje.vida, 100)
+
+#------------------------------------------------------------------------------------------------------------------------
+
+     # test para verificar que el daño calculado es exactamente fuerza + ataque
+    def test_calcular_danio(self):
+        """Verificar que el daño calculado es exactamente fuerza + ataque"""
+        self.assertEqual(self.personaje.calcular_danio(), 15)
+
 #-----------------------------------------------------------------
 
     def test_atacar(self):
-        """Verifica que la funcion atacar funciona correctamente"""
+        """Verificar que el ataque reduce la vida del oponente correctamente"""
         self.personaje.atacar(self.personaje2)
-        self.assertEqual(self.personaje2.vida, 100-(5+10))
         self.assertEqual(self.personaje2.vida, 85)
+    
+    def test_atacar_a_personaje_muerto(self):
+        """Verificar que atacar a un personaje muerto no cambia su vida"""
+        self.personaje.atacar(self.personaje_sin_vida)
+        self.assertEqual(self.personaje_sin_vida.vida, 0)
+    
+    def test_atacar_a_personaje_bloqueando(self):
+        """Verificar que atacar a un personaje bloqueando no cambia su vida"""
+        self.personaje2.bloqueo()
+        self.personaje.atacar(self.personaje2)
+        self.assertEqual(self.personaje2.vida, 100)
+    
+    def test_muerte_por_ataque(self):
+        """Verificar que un ataque puede matar"""
+        self.personaje2.recibir_danio(90)
+        self.personaje.atacar(self.personaje2)
+        self.assertFalse(self.personaje2.estoy_vivo())
+    
