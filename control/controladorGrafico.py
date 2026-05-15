0f3b5158dc69c363ca0e42f0c9ba2c2fe29186e6
elianaiaki <elianajaqueline06@gmail.com>
2026-05-15 15:55:16 -0300

    agregue controlador grafico

---
+++ control/controladorGrafico.py
@@ -0,0 +1,27 @@
+import pygame
+
+class controladorGrafico:
+
+    def __init__(self, pantalla, fuente, jugador1, jugador2):
+        self.pantalla = pantalla
+        self.fuente = fuente
+        self.jugador1 = jugador1
+        self.jugador2 = jugador2
+
+    def dibujar(self, jugador1, jugador2, grafico1, grafico2):
+        self.pantalla.fill((0,0,0))
+
+        grafico1.dibujar(self.pantalla)
+        grafico2.dibujar(self.pantalla)
+
+        # Estado de jugadores
+        estado1 = self.fuente.render(jugador1.mostrar_estado(), True, (255,255,255))
+        estado2 = self.fuente.render(jugador2.mostrar_estado(), True, (255,255,255))
+
+        self.pantalla.blit(estado1, (10, 10))
+        self.pantalla.blit(estado2, (10, 40))
+
+        # Colisión (visual)
+        if grafico1.colisiona_con(grafico2):
+            texto = self.fuente.render("¡COLISIÓN!", True, (255,255,255))
+            self.pantalla.blit(texto, (300, 80))
--- main_principal.py
+++ main_principal.py
@@ -5,6 +5,7 @@ sys.path.append(".")
 from modelo.Jugador import Jugador
 from vista.jugador_grafico import JugadorGrafico
 from control.controlador import Controlador
+from control.controladorGrafico import controladorGrafico
 
 pygame.init()
 
@@ -20,7 +21,7 @@ BLANCO = (255, 255, 255) # Color de texto
 ROJO   = (255, 0, 0)   # Color del jugador 1
 AZUL   = (0, 0, 255)    # Color del jugador 2
 
-#fuente = pygame.font.SysFont(None, 36) # Fuente para mostrar texto en pantalla
+fuente = pygame.font.SysFont(None, 36) # Fuente para mostrar texto en pantalla
 
 # -----------------------------
 # MODELO
@@ -33,6 +34,7 @@ jugador2 = Jugador("Jugador 2", 100, 8, 6, "piña")
 # -----------------------------
 grafico1 = JugadorGrafico(100, 300, ROJO, jugador1)
 grafico2 = JugadorGrafico(400, 300, AZUL, jugador2)
+controladorGrafico = controladorGrafico(pantalla, fuente, jugador1, jugador2)
 
 # -----------------------------
 # CONTROLADOR
@@ -40,11 +42,11 @@ grafico2 = JugadorGrafico(400, 300, AZUL, jugador2)
 controlador = Controlador(grafico1, grafico2, ANCHO, ALTO)
 
 reloj = pygame.time.Clock()
-fuente = pygame.font.SysFont(None, 36)
 
 # -----------------------------
 # BUCLE PRINCIPAL
-# -----------------------------
+# ---------------
+#--------------
 
 while controlador.corriendo:
     # -----------------------------
@@ -60,29 +62,7 @@ while controlador.corriendo:
     # -----------------------------
     # DIBUJO
     # -----------------------------
-    pantalla.fill(NEGRO)
-
-    grafico1.dibujar(pantalla)
-    grafico2.dibujar(pantalla)
-
-    # Colisión (visual)
-    if grafico1.colisiona_con(grafico2):
-        texto = fuente.render("¡COLISIÓN!", True, BLANCO)
-        pantalla.blit(texto, (300, 80))
-
-    # Vida en pantalla
-    # texto1 = fuente.render(f"J1 Vida: {jugador1.vida}", True, BLANCO)
-    # texto2 = fuente.render(f"J2 Vida: {jugador2.vida}", True, BLANCO)
-
-    # pantalla.blit(texto1, (10, 10))
-    # pantalla.blit(texto2, (10, 40))
-
-    # Estado de jugadores
-    estado1 = fuente.render(jugador1.mostrar_estado(), True, BLANCO)
-    estado2 = fuente.render(jugador2.mostrar_estado(), True, BLANCO)
-
-    pantalla.blit(estado1, (10, 10))
-    pantalla.blit(estado2, (10, 40))
+    controladorGrafico.dibujar(jugador1, jugador2, grafico1, grafico2)
 
     pygame.display.flip()
     reloj.tick(60)
